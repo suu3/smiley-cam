005d2dc9b87b24a9553a9d162f7ff3b0e26cbeb2
suu3 <71166763+suu3@users.noreply.github.com>
2021-08-27 23:49:10 +0900

    #10.1

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { Camera, Permissions } from "expo"; // Permission은 유저에게 허락받으려고
+export default class App extends React.Component {
+  state = {
+    hasPermission: null
+  };
+  componentDidMount = async() => {
+    const {status} = await Permissions.askAsync(Permission.CAMERA);//카메라에 대한 허락을 물음
+    console.log(status);
+    if(status === "granted"){
+      this.setState({hasPermission: true});
+    } else{
+      this.setState({hasPermission: false});
+    }
+  };
+  render() {
+    const { hasPermission } = this.state;
+    if (hasPermission === true) {
+      return (
+        <View>
+          <Text>Has permissions</Text>
+        </View>
+      );
+    } else if (hasPermission === false) {
+      return (
+        <View>
+          <Text>Don't have permission for this</Text>
+        </View>
+      );
+    } else {
+      return <ActivityIndicator />;
+    }
+  }
 }
 
 const styles = StyleSheet.create({
